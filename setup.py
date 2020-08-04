4cd85b83c7f1b428f8fa6a0d8d9c45413274e9f5
Felix Gravila <felixgravila@gmail.com>
2020-08-04 13:11:53 +0200

    Cookiecutter first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML project to generate a beat map from audio files',
+    author='Felix Gravila',
+    license='MIT',
+)
